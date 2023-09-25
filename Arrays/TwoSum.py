3d651b97b6e7ba96da78e2a14a7b7a0d18c1d777
jihane01 <jihanemajd14@gmail.com>
2023-09-25 11:35:23 +0100

    add the final function

---
+++ Arrays/TwoSum.py
@@ -0,0 +1,26 @@
+#Approach using a hash table:
+
+#1:Create an empty hash table to store elements and their indices.
+#2: Iterate through the array from left to right.
+#3:For each element nums[i], calculate the complement by subtracting it from the target: complement = target - nums[i].
+#4:Check if the complement exists in the hash table. If it does, we have found a solution.
+#5:If the complement does not exist in the hash table, add the current element nums[i] to the hash table with its index as the value.
+#6:Repeat steps 3-5 until we find a solution or reach the end of the array.
+#7:If no solution is found, return an empty array or an appropriate indicator.
+
+def two_sum(nums, target):
+    d = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in d:
+            return [d[complement],i]
+        d[nums[i]] = i
+    return [-1,-1]
+
+
+#Test Function
+nums = [1,3,5,4]
+target = 8
+
+result =two_sum(nums,target)
+print(result)
