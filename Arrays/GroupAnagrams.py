b1940a712a829c583b24c1e3a7a21f8678cd1fbb
jihane01 <jihanemajd14@gmail.com>
2023-09-25 12:14:48 +0100

    change

--- Arrays/GroupAnagrams.py
+++ Arrays/GroupAnagrams.py
@@ -4,9 +4,14 @@
 #i add them to a hash table the sorted every element alphabitcally 
 # compare element li equals and print them 
 
-
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d=collections.defaultdict(list)
-        [d[''.join(sorted(i))].append(i) for i in strs]
-        return d.values()
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
+    
+      
