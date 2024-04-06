e4054db06199d8e49d77582c3353b9a1305bbc50
jihane01 <jihanemajd14@gmail.com>
2024-04-06 10:12:52 +0000

    add brute force solution

--- Arrays/ContainsDuplicate.py
+++ Arrays/ContainsDuplicate.py
@@ -8,3 +8,15 @@ def containsDuplicate(nums):
 ''' what i did is that i try to sort the arrays with another list and then iterate to find the first equal elemnts so return True otherwise retur False but the sort funct
 ion have a O(n log n)'''
 
+##Brute Force ##
+def containsDuplicat(nums):
+    n = len(nums)
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if  nums[i] == nums[j]:
+                return True
+    return False
+##Testing Function 
+answer = containsDuplicat([2,1,3,2])
+print(answer)
+
